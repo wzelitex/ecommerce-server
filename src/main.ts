cc336378aa68c0dff882fb02d98d43dc15b85baa
wzelitex <wzelitexprogramming23@gmail.com>
2025-05-27 23:24:14 -0600

    urls added

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000',
+      'https://www.ecommerce-business.com.mx',
+      'https://www.ecommerce-go.com.mx/',
       'http://localhost:3001',
-      'http://localhost:3002',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
